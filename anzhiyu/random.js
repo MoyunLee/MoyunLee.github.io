3360dfc6b714805d8168651456004119ca3a299c
moyunlee <2106996916@qq.com>
2025-03-28 18:37:24 +0800

    Site updated: 2025-03-28 18:37:24

--- anzhiyu/random.js
+++ anzhiyu/random.js
@@ -1,3 +1,3 @@
-var posts=["2025/03/28/hello-world/","2024/11/04/每周大学习——初步学习HTML/"];function toRandomPost(){
+var posts=["posts/hello-world.html","posts/每周大学习——初步学习HTML.html"];function toRandomPost(){
     pjax.loadUrl('/'+posts[Math.floor(Math.random() * posts.length)]);
   };
