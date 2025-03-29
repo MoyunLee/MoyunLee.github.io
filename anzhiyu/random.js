a3c42a6196c131d9fcebccfec31c9273c6fb40bc
moyunlee <2106996916@qq.com>
2025-03-29 15:40:58 +0800

    Site updated: 2025-03-29 15:40:58

--- anzhiyu/random.js
+++ anzhiyu/random.js
@@ -1,3 +1,3 @@
-var posts=["posts/hello-world.html"];function toRandomPost(){
+var posts=["posts/hello-world.html","posts/C语言.html"];function toRandomPost(){
     pjax.loadUrl('/'+posts[Math.floor(Math.random() * posts.length)]);
   };
